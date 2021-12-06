b82802a839ef4048240be75b3ae0635bd72b39fb
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-12-06 19:05:50 +0330

    Adding initial docstring

---
+++ preprocessing.py
@@ -0,0 +1,8 @@
+"""
+    This prrprocessing script performs the followings:
+        * Scaling all volumes using MinMaxScalar
+        * Combining the three volumes(T1, MD, rCBV) into single multi-channel volume
+        * Saving volumes as numpy arrays(.npy)
+
+    Author: Milad Sadeghi DM - EverLookNeverSee@GitHub
+"""
