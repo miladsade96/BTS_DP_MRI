ebc54eb57383eadd202c8fa79d21b1b2b1f09a85
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-12-13 20:51:57 +0330

    Defining and implementing private load image function

--- image_data_generator.py
+++ image_data_generator.py
@@ -8,3 +8,19 @@ import os
 import glob
 import numpy as np
 from typing import List, Tuple
+
+
+def _load_images(img_list: List) -> Tuple:
+    """
+    This private function loads the numpy array image files
+    that are passed within input parameter and append them into a list
+    :param img_list: List, Contains absolute paths to .npy files
+    :return: Tuple, Loaded images
+    """
+    images = list()
+    for image in img_list:
+        if image.split(".")[1] == "npy":
+            npy_image = np.load(image)
+            images.append(npy_image)
+    images = np.array(images)
+    return tuple(images)
