35f3a2ac99f6c98366f480ea7b82fe1e2abc01e2
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-12-14 20:22:59 +0330

    Fixing generated .npy files shape

--- preprocessing.py
+++ preprocessing.py
@@ -31,7 +31,7 @@ mm_scaler = MinMaxScaler()
 t1_list = sorted(glob.glob(f"{args.dataset}/Train/*/*T1.nii"))
 MD_list = sorted(glob.glob(f"{args.dataset}/Train/*/*MD.nii"))
 rCBV_list = sorted(glob.glob(f"{args.dataset}/Train/*/*rCBV.nii"))
-Ann_list = sorted(glob.glob(f"{args.dataset}/Train/*/*Ann.nii"))
+mask_list = sorted(glob.glob(f"{args.dataset}/Train/*/*mask.nii"))
 
 
 PATH = os.path.abspath(f"{args.dataset}/")      # Getting absolute path
@@ -44,35 +44,43 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))    # Navigating back to pro
 
 
 for i, _ in enumerate(t1_list):
+    # Adding six zero slices on axis 2 in order to feed .npy file into U-Net model
+    # image files shape: (128, 128, 16, 3) and mask files shape: (128, 128, 16, 4)
+    zeros = np.zeros((128, 128, 6))
+
     if args.verbose:
         print("Now preparing image and masks number: ", i)
 
     temp_image_t1 = nib.load(os.path.abspath(t1_list[i])).get_fdata()
     temp_image_t1 = mm_scaler.fit_transform(temp_image_t1.reshape(-1, temp_image_t1.shape[-1])).reshape(
         temp_image_t1.shape)
+    temp_image_t1 = np.append(temp_image_t1, zeros, axis=2)
     if args.verbose:
         print(f"T1 for sample number {i} Loaded and rescaled.")
     temp_image_MD = nib.load(os.path.abspath(MD_list[i])).get_fdata()
     temp_image_MD = mm_scaler.fit_transform(temp_image_MD.reshape(-1, temp_image_MD.shape[-1])).reshape(
         temp_image_MD.shape)
+    temp_image_MD = np.append(temp_image_MD, zeros, axis=2)
     if args.verbose:
         print(f"MD for sample number {i} Loaded and rescaled.")
     temp_image_rCBV = nib.load(os.path.abspath(rCBV_list[i])).get_fdata()
     temp_image_rCBV = mm_scaler.fit_transform(temp_image_rCBV.reshape(-1, temp_image_rCBV.shape[-1])).reshape(
         temp_image_rCBV.shape)
+    temp_image_rCBV = np.append(temp_image_rCBV, zeros, axis=2)
     if args.verbose:
         print(f"rCBV for sample number {i} Loaded and rescaled.")
-    temp_Ann = nib.load(os.path.abspath(Ann_list[i])).get_fdata()
-    temp_Ann = temp_Ann.astype(np.uint8)
+    temp_mask = nib.load(os.path.abspath(mask_list[i])).get_fdata()
+    temp_mask = np.append(temp_mask, zeros, axis=2)
+    temp_mask = temp_mask.astype(np.uint8)
     if args.verbose:
-        print(f"Ann for sample number {i} Loaded and converted to uint8.")
+        print(f"Mask for sample number {i} Loaded and converted to uint8.")
     temp_combined_images = np.stack([temp_image_t1, temp_image_MD, temp_image_rCBV], axis=3)
     if args.verbose:
         print(f"T1, MD and rCBV volumes combined as a single MegaVolume.")
 
     output = os.path.abspath("dataset/npy_files/")
 
-    temp_Ann = to_categorical(temp_Ann, num_classes=4)
+    temp_mask = to_categorical(temp_mask, num_classes=4)
 
     if os.path.isfile(f"{output}/{i}/image_" + str(i) + ".npy"):
         if args.verbose:
@@ -82,10 +90,10 @@ for i, _ in enumerate(t1_list):
         if args.verbose:
             print(f"Number {i} image .npy files saved successfully.")
 
-    if os.path.isfile(f"{output}/{i}/ann_" + str(i) + ".npy"):
+    if os.path.isfile(f"{output}/{i}/mask_" + str(i) + ".npy"):
         if args.verbose:
-            print(f"Annotation file already exists.")
+            print(f"Mask file already exists.")
     else:
-        np.save(f"{output}/{i}/ann_" + str(i) + ".npy", temp_Ann)
+        np.save(f"{output}/{i}/mask_" + str(i) + ".npy", temp_mask)
         if args.verbose:
-            print(f"Number {i} annotation .npy files saved successfully.")
+            print(f"Number {i} mask .npy files saved successfully.")
