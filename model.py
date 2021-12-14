6c9a2229b29171fe22a356a899fc0a2c4564647c
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-12-14 22:04:08 +0330

    Importing relevant packages

--- model.py
+++ model.py
@@ -2,3 +2,7 @@
     3D U-Net Model
     Author: Milad Sadeghi DM - EverLookNeverSee@GitHub
 """
+
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv3D, Conv3DTranspose, Dropout, MaxPooling3D, concatenate
