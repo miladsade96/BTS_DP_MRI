21a7a1cf1afc3475e6f75bc7fbfb73a0f7f4aeae
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-12-15 19:52:01 +0330

    Optimizing import statements

--- model.py
+++ model.py
@@ -4,5 +4,8 @@
 """
 
 
+from typing import Tuple
 from tensorflow.keras.models import Model
+from tensorflow.python.types.core import Tensor
+from tensorflow.keras.activations import relu, softmax
 from tensorflow.keras.layers import Input, Conv3D, Conv3DTranspose, Dropout, MaxPooling3D, concatenate
