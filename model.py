1f2d182fd6b91a05d19f9e97315c96328dbb2a25
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-12-15 19:56:26 +0330

    Defining and implementing build bridge block private function

--- model.py
+++ model.py
@@ -29,3 +29,21 @@ def _build_encoder_block(pl: Tensor, n_filters: int, k_size: Tuple = (3, 3, 3), 
     c_2 = Conv3D(filters=n_filters, kernel_size=k_size, kernel_initializer=ki, activation=af, padding="same")(do)
     mp = MaxPooling3D(pool_size=p_size)(c_2)
     return mp, c_2  # c_2 is skip connection that will be connected to corresponding upsampling block in decoder path
+
+
+def _build_bridge_block(pl: Tensor, n_filters: int = 256, k_size: Tuple = (3, 3, 3),
+                        af=relu, ki: str = "he_uniform", drop_rate: float = 0.3):
+    """
+    Bridge path convolution block builder
+    :param pl: Tensor, Previous layer
+    :param n_filters: Integer, Number of filters in convolution layer, default is 256
+    :param k_size: Tuple, Kernel size in convolution layer, default is (3, 3, 3)
+    :param af: Activation function in convolution layer, default is relu
+    :param ki: String, Layer weight initializer algorithm, default is he_uniform
+    :param drop_rate: Float, Dropout layer rate, default is 0.3
+    :return: Tensor
+    """
+    c_1 = Conv3D(filters=n_filters, kernel_size=k_size, activation=af, kernel_initializer=ki, padding="same")(pl)
+    do = Dropout(rate=drop_rate)(c_1)
+    c_2 = Conv3D(filters=n_filters, kernel_size=k_size, activation=af, kernel_initializer=ki, padding="same")(do)
+    return c_2
