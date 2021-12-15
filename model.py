e54f229507a4dfb3cc43076fe671e4a4a77fad11
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-12-15 19:57:09 +0330

    Defining and implementing build decoder block private function

--- model.py
+++ model.py
@@ -47,3 +47,26 @@ def _build_bridge_block(pl: Tensor, n_filters: int = 256, k_size: Tuple = (3, 3,
     do = Dropout(rate=drop_rate)(c_1)
     c_2 = Conv3D(filters=n_filters, kernel_size=k_size, activation=af, kernel_initializer=ki, padding="same")(do)
     return c_2
+
+
+def _build_decoder_block(pl: Tensor, sc: Tensor, n_filters: int, drop_rate: float, k_size_tr: Tuple = (2, 2, 2),
+                         k_size: Tuple = (3, 3, 3), stride: Tuple = (2, 2, 2), af=relu, ki: str = "he_uniform"):
+    """
+    Decoder path Upsampling block builder
+    :param pl: Tensor, Previous layer
+    :param sc: Tensor, Skip connection returned from corresponding downsampling block
+    :param n_filters: Integer, Number of filters in Conv3DTranspose and Con3D
+    :param k_size_tr: Tuple, Kernel size in Conv3DTranspose layer, default is (2, 2, 2)
+    :param k_size: Tuple, Kernel size in Conv3D layer, default is (3, 3, 3)
+    :param stride: Tuple, Strides size in Conv3DTranspose layer, default is (2, 2, 2)
+    :param af: Activation function in Conv3D layer, default is relu
+    :param ki: String, Layer weight initializer algorithm, default is he_uniform
+    :param drop_rate: Float, Dropout layer rate
+    :return: Tensor
+    """
+    ct = Conv3DTranspose(filters=n_filters, kernel_size=k_size_tr, strides=stride, padding="same")(pl)
+    concat = concatenate([ct, sc], axis=4)
+    c_1 = Conv3D(filters=n_filters, kernel_size=k_size, activation=af, kernel_initializer=ki, padding="same")(concat)
+    do = Dropout(rate=drop_rate)(c_1)
+    c_2 = Conv3D(filters=n_filters, kernel_size=k_size, activation=af, kernel_initializer=ki, padding="same")(do)
+    return c_2
