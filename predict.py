45384b7a04f9f1402f98293431e4c95bc7376528
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-12-29 19:35:22 +0330

    Importing relevant packages

--- predict.py
+++ predict.py
@@ -2,3 +2,12 @@
     Loading trained model and make some predictions on unseen data
     @author: Milad Sadeghi DM - EverLookNeverSee@GitHub
 """
+
+
+import numpy as np
+import nibabel as nb
+import matplotlib.pyplot as plt
+from tensorflow.keras.metrics import MeanIoU
+from tensorflow.keras.models import load_model
+from segmentation_models_3D.metrics import iou_score
+from segmentation_models_3D.losses import DiceLoss, categorical_focal_loss
