d1e7fd4b22626acd331eae1abdb19ee6954bfbdc
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-01-18 17:51:57 +0330

    Reordering arguments

--- predict.py
+++ predict.py
@@ -14,10 +14,11 @@ from tensorflow.keras.models import load_model
 # Initializing cli argument parser
 parser = argparse.ArgumentParser()
 # Adding the arguments
-parser.add_argument("--image", help="Path to preprocessed .npy image file")
-parser.add_argument("--mask", help="Path to preprocessed .npy mask file")
 parser.add_argument("--model", help="Path to saved model")
+parser.add_argument("--mask", help="Path to preprocessed .npy mask file")
+parser.add_argument("--image", help="Path to preprocessed .npy image file")
 parser.add_argument("-v", "--verbose", help="Level of verbosity", action="store_true")
 parser.add_argument("-s", "--save_plot", help="Save Prediction plot", default=os.getcwd())
+parser.add_argument("--slice", help="Predicted mask lice number in order to plot", type=int, default=6)
 # Parsing the arguments
 args = parser.parse_args()
