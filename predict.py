dcedc3c23f63bda31bafc9d6209163a2c26db54e
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-01-18 17:26:48 +0330

    Setting cli arguments

--- predict.py
+++ predict.py
@@ -3,10 +3,21 @@
     @author: Milad Sadeghi DM - EverLookNeverSee@GitHub
 """
 
+
+import os
 import argparse
 import numpy as np
-import nibabel as nb
-from glob import glob
-from os.path import abspath
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
+
+
+# Initializing cli argument parser
+parser = argparse.ArgumentParser()
+# Adding the arguments
+parser.add_argument("--image", help="Path to preprocessed .npy image file")
+parser.add_argument("--mask", help="Path to preprocessed .npy mask file")
+parser.add_argument("--model", help="Path to saved model")
+parser.add_argument("-v", "--verbose", help="Level of verbosity", action="store_true")
+parser.add_argument("-s", "--save_plot", help="Save Prediction plot", default=os.getcwd())
+# Parsing the arguments
+args = parser.parse_args()
