f7e9f33516c18edfcae3a9f9b5c6ce616433d51e
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-01-22 19:07:44 +0330

    Setting cli arguments

--- reporter.py
+++ reporter.py
@@ -6,4 +6,16 @@
 
 import argparse
 import pandas as pd
+from os import getcwd
 from os.path import abspath
+
+
+# Initializing cli argument parser
+parser = argparse.ArgumentParser()
+# Adding arguments
+parser.add_argument("-f", "--file_path", help="Path to .csv file directory")
+parser.add_argument("-v", "--verbose", action="store_true", help="Level of verbosity")
+parser.add_argument("-s", "--save", help="Path to save report.csv file", default=getcwd())
+
+# Parsing the arguments
+args = parser.parse_args()
